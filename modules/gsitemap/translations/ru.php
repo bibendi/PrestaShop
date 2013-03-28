208223251615ac3211baa86486bbb7f2593983bd
Merkushin <merkushin.m.s@gmail.com>
2013-03-28 21:55:12 +0600

    add translations

---
+++ modules/gsitemap/translations/ru.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{gsitemap}prestashop>gsitemap_3aaaafde6f9b2701f9e6eb9292e95521'] = 'Карта сайта Google sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_ea5d9ad08653eae5509e2fc70171a0d6'] = 'Сгенерировать файл Google sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_ecf80a15b9595df825e451eb4def853b'] = 'Не могу создать файл %ssitemap.xml .';
+$_MODULE['<{gsitemap}prestashop>gsitemap_0ce45c4c029ee2c598033b5c3a7fdadd'] = 'Карта сайта создана';
+$_MODULE['<{gsitemap}prestashop>gsitemap_401149c95366caa82ed416edb5eb66a3'] = 'Ошибка во время создания файла sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_dbca7bedb74f116f17eea44cddf9761f'] = 'Индекс Sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_eb175747116b689caadea1b84202f513'] = 'Ваш файл Google sitemap находится по следующему адресу:';
+$_MODULE['<{gsitemap}prestashop>gsitemap_4851323bb81fbc4aa65c1b37af8a1d1d'] = 'Карта сайта для:';
+$_MODULE['<{gsitemap}prestashop>gsitemap_39d616ecc73e174052f1877ac2b3c853'] = 'Обновить:';
+$_MODULE['<{gsitemap}prestashop>gsitemap_b908c2f34052b5276e0bf50f0e042211'] = 'Размер файла:';
+$_MODULE['<{gsitemap}prestashop>gsitemap_3f69f5c6c3c90eee482f28ae77390cb9'] = 'Проиндексировано страниц:';
+$_MODULE['<{gsitemap}prestashop>gsitemap_1c29b1fc86e14218feccb4bf5e3c3331'] = 'Карта сайта также включает товары отключенных категорий';
+$_MODULE['<{gsitemap}prestashop>gsitemap_5f93333d7d018796366e82a1fa763d47'] = 'Карта сайта также включает страницы, которые не находятся в блоке CMS';
+$_MODULE['<{gsitemap}prestashop>gsitemap_ed238c7f4f41153f6f7e02041d193381'] = 'Сгенерировать файл sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_0a376e01c0139491a901b3f965a6978d'] = 'Обновит файл sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_be35982b0637fe74565dde00768e3534'] = 'Используйте задачи cron для пересоздания sitemap';
+$_MODULE['<{gsitemap}prestashop>gsitemap_f1c0f389e0aade719b421f28bad33807'] = 'Оптимизация для поисковых систем';
+$_MODULE['<{gsitemap}prestashop>gsitemap_4ff2e716a7d06ce5274b4090b39abad3'] = 'Посмотрите';
+$_MODULE['<{gsitemap}prestashop>gsitemap_5e4ace6a96971949d519e8b1885652aa'] = 'эту информацию';
+$_MODULE['<{gsitemap}prestashop>gsitemap_d13de4a83d7b44c2e65495def8ef1c7b'] = 'для получения более подробной информации';
